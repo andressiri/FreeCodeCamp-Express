2545dbacefc1bcfc94065caf5fad6a97eb999f10
Andrés Siri <andres.siri@hotmail.com>
2021-09-10 18:42:29 -0300

    challenge 3 - send the /views/index.html file as response toget request to the / path

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ var app = express();
 
 console.log("Hello World");
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    //res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
