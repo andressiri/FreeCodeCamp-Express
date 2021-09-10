38db03e29ded3f57119093e33f1b29702005bc39
Andrés Siri <andres.siri@hotmail.com>
2021-09-10 19:04:57 -0300

    challenge 4 - Mount the express.static() middleware to the path /public with app.use()

--- myApp.js
+++ myApp.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
-app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+    //res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
