13adbcef4ee6f49631d1aabe323ca9a650439db9
Andrés Siri <andres.siri@hotmail.com>
2021-09-10 19:34:34 -0300

    Serve the object {message: Hello json} as a response, in JSON format, to GET requests to the /json route

--- myApp.js
+++ myApp.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
 console.log("Hello World");
 app.get("/", (req, res) => {
-    //res.send("Hello Express");
-    res.sendFile(__dirname + "/views/index.html");
+  //res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+  res.json({
+    "message": "Hello json"
+  });
+});
+
+
 
 
 
