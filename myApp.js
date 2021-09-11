225c5e75889956bccd425c8c67cf743e3b26f2d4
Andrés Siri <andres.siri@hotmail.com>
2021-09-10 22:52:51 -0300

    challenge 8 - In the route app.get('/now', ...) chain a middleware function and the final handler

--- myApp.js
+++ myApp.js
@@ -24,6 +24,16 @@ app.get("/json", (req, res) => {
   });
 });
 
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+},
+(req, res) => {
+  res.send({
+    time: req.time
+  });
+});
+
 
 
 
